3efd14a77498fa43b2c872c600e806a3e15562c4
Pedro Silva <pedro.silva@mindera.com>
2022-11-06 22:02:50 +0000

    first commit

---
+++ argocd/README.md
@@ -0,0 +1,4 @@
+install argocd by running:
+- helm repo add argo https://argoproj.github.io/argo-helm
+- helm install argocd argo/argo-cd -n argocd
+
---
+++ infra-terraform/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.0"
+    }
+  }
+}
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+resource "kubernetes_namespace" "argocd_namespace_creation" {
+  metadata {
+    name = "argocd"
+  }
+}
