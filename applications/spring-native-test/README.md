0dd322b716abeb941f8dfd847d082d898849d485
Pedro Silva <pedro.silva@mindera.com>
2022-11-08 13:59:58 +0000

    update spring native app

---
+++ applications/spring-native-test/README.md
@@ -0,0 +1,3 @@
+DOCKER_BUILDKIT=0 COMPOSE_DOCKER_CLI_BUILD=0 docker build -t jpsilva/spring-native-test:0.0.1 .
+
+docker push jpsilva/spring-native-test:0.0.1 
---
+++ applications/spring-native-test/src/main/java/com/example/springnativetest/SpringNativeTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springnativetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNativeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNativeTestApplication.class, args);
+	}
+
+}
